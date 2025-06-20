4413b8db69902b347689721863d975cb31391a80
Yakir Adela <yakirade@gmail.com>
2025-06-20 22:40:26 +0300

    Create main.tf

---
+++ distributed-task-manager/infra/main.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source           = "./vpc"
+  vpc_name         = var.vpc_name
+  vpc_cidr         = var.vpc_cidr
+  azs              = var.azs
+  public_subnets   = var.public_subnets
+  private_subnets  = var.private_subnets
+  environment      = var.environment
+}
+
+module "eks" {
+  source          = "./eks"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  vpc_id          = module.vpc.vpc_id
+  private_subnets = module.vpc.private_subnets
+  environment     = var.environment
+}
+
+module "rds" {
+  source            = "./rds"
+  db_username       = var.db_username
+  db_password       = var.db_password
+  private_subnets   = module.vpc.private_subnets
+  security_group_id = module.vpc.default_security_group_id # נדרש לוודא שה-VPC מחזיר את הפלט הזה
+}
