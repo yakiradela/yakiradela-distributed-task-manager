ed4e1332b0e3a39b8952e30616780090042fb427
Yakir Adela <yakirade@gmail.com>
2025-06-20 20:46:06 +0300

    Update variables.tf

--- distributed-task-manager/infra/boostrap/variables.tf
+++ distributed-task-manager/infra/boostrap/variables.tf
@@ -16,3 +16,7 @@ variable "lock_table_name" {
     default     = "dtm-terraform-state" 
 }
 
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
