f8e8dae8088da0c2d3aa857fa0b149d8b9d1afec
Yakir Adela <yakirade@gmail.com>
2025-06-20 14:25:44 +0300

    Update config.py

--- distributed-task-manager/services/api-gateway/src/config.py
+++ distributed-task-manager/services/api-gateway/src/config.py
@@ -1,6 +1,6 @@
 import os
 
-class Congif:
+class Config:
     USERS_SERVICE_URL = os.getenv("USERS_SERVICE_URL", "http://users-service")
     TASKS_SERVICE_URL = os.getenv("TASKS_SERVICE_URL", "http://tasks-service")
     SECRET_KEY = os.getenv("SECRET_KEY", "super-secret")
