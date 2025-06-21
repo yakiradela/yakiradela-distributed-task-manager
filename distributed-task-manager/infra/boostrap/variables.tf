79d84fd5ccf73169e489ee8995d9e422a54a3644
Yakir Adela <yakirade@gmail.com>
2025-06-21 15:55:58 +0300

    Update variables.tf

--- distributed-task-manager/infra/boostrap/variables.tf
+++ distributed-task-manager/infra/boostrap/variables.tf
@@ -1,22 +1,17 @@
-variable "aws_ragion" {
-    description = "AWS region to deploy resources in"
-    type        = string
-    default     = "us-east-2" 
-}
-
-variable "bucket_name" {
-    description = "Name of the S3 bucket to store Terraform state"
-    type        = string
-    default     = "dtm-terraform-state" 
-}
-
-variable "lock_table_name" {
-    description = "Name of the S3 bucket to store Terraform state"
-    type        = string
-    default     = "dtm-terraform-state" 
-}
-
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
+  default     = "us-east-2"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket to store Terraform state"
+  type        = string
+  default     = "dtm-terraform-state"
+}
+
+variable "lock_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locking"
+  type        = string
+  default     = "dtm-terraform-locks"
 }
