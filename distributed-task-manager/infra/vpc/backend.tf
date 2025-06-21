b7fc5da37629a212bb2badd37bacf4ae80c882ca
Yakir Adela <yakirade@gmail.com>
2025-06-21 15:46:51 +0300

    Update backend.tf

--- distributed-task-manager/infra/vpc/backend.tf
+++ distributed-task-manager/infra/vpc/backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "dtm-terraform-state"
-    key            = "vpc/terraform.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "dtm-terraform-locks"
-    encrypt        = true
-  }
-}
+
