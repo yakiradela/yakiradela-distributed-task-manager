bf88772ddc2c9735724c3cf287ad87fb46758335
Yakir Adela <yakirade@gmail.com>
2025-06-21 15:45:59 +0300

    Update main.tf

--- distributed-task-manager/infra/eks/main.tf
+++ distributed-task-manager/infra/eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
     source              = "terraform-aws-modules/eks/aws"
-    version             = "20.8.4"
+    version             = "19.17.0"
 
     cluster_name        = var.cluster_name
     cluster_version     = var.cluster_version 
