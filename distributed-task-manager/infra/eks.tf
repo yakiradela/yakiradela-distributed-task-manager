60b1df72fc19acba1febc9470cb2c76614a34bf9
Yakir Adela <yakirade@gmail.com>
2025-06-22 15:57:34 +0300

    Update eks.tf

--- distributed-task-manager/infra/eks.tf
+++ distributed-task-manager/infra/eks.tf
@@ -1,14 +1,1 @@
-resource "random_id" "kms_suffix" {
-  byte_length = 4
-}
 
-resource "aws_kms_key" "eks" {
-  description             = "KMS key for EKS secrets encryption"
-  deletion_window_in_days = 10
-  enable_key_rotation      = true
-}
-
-resource "aws_kms_alias" "eks" {
-  name          = "alias/eks/${var.cluster_name}-${random_id.kms_suffix.hex}"
-  target_key_id = aws_kms_key.eks.key_id
-}
